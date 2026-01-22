7782388ddd4ae198ca611f1fc59250cf38ea5c44
Valeriy Manzhos <47538801+InvisibleRain@users.noreply.github.com>
2026-01-22 16:58:28 +0300

    Revert "Use string interpolation instead of string concatenation to avoid type errors in get_html"
    This reverts commit cd8d21f7cfca605ad269c42b5dc89624d2ffa3da.

--- parser_cls.py
+++ parser_cls.py
@@ -567,9 +567,9 @@ class AvitoParse:
                 "is_mobile": False,
                 "has_touch": False,
             }
-            if isinstance(self.config.playwright_state_file,str) and self.config.playwright_state_file != "":
-                context_args["storage_state"] = self.config.playwright_state_file
-                logger.debug(f"Используем Playwright state file {self.config.playwright_state_file}")
+            if isinstance(config.playwright_state_file,str):
+                context_args["storage_state"] = config.playwright_state_file
+                logger.debug("Используем Playwright state file " + config.playwright_state_file)
             else:
                 logger.debug("Playwright state file не задан. Используем пустой контекст Playwright.")
             if self.proxy_split_obj:
@@ -600,9 +600,9 @@ class AvitoParse:
                     state_filepath = Path(state_file)
                     state_filepath.touch(mode=0o600, exist_ok=True) # Set mode to protect sensitive cookies
                     storage = await context.storage_state(path=state_filepath)
-                    logger.info(f"Сессия пользователя Авито сохранена в {state_file}")
+                    logger.info("Сессия пользователя Авито сохранена в " + state_file)
                 except:
-                    logger.error(f"Не удалось записать сессию в файл {state_file}")
+                    logger.error("Не удалось записать сессию в файл " + state_file)
 
             return await page.content()
 
