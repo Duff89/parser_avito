fad8977c21c25886b6afe06d4650a7d13ebe4545
Valeriy Manzhos <47538801+InvisibleRain@users.noreply.github.com>
2026-01-22 16:32:09 +0300

    Revert "Use a more ambigous log message when saving playwright state"
    This reverts commit 0fdf6e7ae7891043fc971ba7078582ad61220cbf.

--- parser_cls.py
+++ parser_cls.py
@@ -616,7 +616,7 @@ class AvitoParse:
                     state_filepath = Path(state_file)
                     state_filepath.touch(mode=0o600, exist_ok=True) # Set mode to protect sensitive cookies
                     storage = await context.storage_state(path=state_filepath)
-                    logger.debug(f"Playwright state сохранён в {state_file}")
+                    logger.info(f"Сессия пользователя Авито сохранена в {state_file}")
                 except:
                     logger.error(f"Не удалось записать сессию в файл {state_file}")
 
